4d69cc8f9fce65df5f05a0d3e782ab0e2e566a22
Sample123 <119918517+kou4869@users.noreply.github.com>
2023-06-11 23:41:15 +0900

    tuika

---
+++ hello.java
@@ -0,0 +1,10 @@
+
+public class hello {
+  // TODO 自動生成されたメソッド・スタブ
+  // 画面に「Hello Java！」と表示
+  public static void main(String[] args) {
+    System.out.println("Hello Java!");
+		System.out.println("こんにちは");
+		System.out.println("世界");
+  }
+}
