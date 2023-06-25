d138fb589078d7adf186831928dc4dd477693ef0
Sample123 <119918517+kou4869@users.noreply.github.com>
2023-06-25 23:19:17 +0900

    tuika

--- hairetu4.java
+++ hairetu4.java
@@ -1,7 +1,17 @@
 public class hairetu4 {
   public static void main(String[] args) {
 
-    
-  }
-  
+    int[] scores = new int[3];
+		scores[0] = 90;
+		scores[1] = 80;
+		scores[2] = 70;
+		//scores[3] = 10;	//コメント外すとエラー
+		
+		scores = null;
+		
+		//scores[0] = 10;	//コメント外すとエラー
+		
+
+	}
 }
+
